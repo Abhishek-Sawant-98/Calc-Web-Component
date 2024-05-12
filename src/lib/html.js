eb0d7f03a707d0edaadf60b6da0cf05cb3d4c8b3
Abhishek Sawant <abhishek.sawant@analog.com>
2024-05-12 16:00:16 +0530

    feat: adding utility files

---
+++ src/lib/html.js
@@ -0,0 +1,35 @@
+import { createSheet } from './css';
+
+/**
+ * Creates a template element for use in the ShadowDOM
+ * @param {string} html HTML string
+ * @returns {HTMLTemplateElement} Template to be used
+ */
+export const createTemplate = ( html ) => {
+	const template = document.createElement( 'template' );
+	template.innerHTML = html;
+	return template.content.cloneNode( true );
+};
+
+/**
+ * Creates a shadow root (expected to be executed with `.call()` method)
+ * @param {string} html HTML string for the template
+ * @param {string} css CSS string for the stylesheet
+ * @param {object} [config] Additional configuration for the shadow root
+ */
+export function createShadowRoot( html, css, config = {} ) {
+	this.root = this.attachShadow( { mode: 'open', ...config } );
+	this.root.appendChild( createTemplate( html ) );
+	this.root.adoptedStyleSheets = createSheet( css );
+}
+
+/**
+ * Queries elements and returns an array instead of a node list
+ * @param {string} selector Query selector string to be retrieved
+ * @returns {Array} Array of elements
+ */
+export function querySelectorArray( selector ) {
+	return Array.from(
+		this.root.querySelectorAll( selector ),
+	);
+}
